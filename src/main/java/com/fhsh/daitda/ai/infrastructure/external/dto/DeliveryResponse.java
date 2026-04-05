da645b68bcf84122798f54fe28819f2839b64482
hil679 <hil679922@gmail.com>
2026-04-06 01:32:21 +0900

    Feat: add DeliveryFeignClient to deliver routes info for ai

---
+++ src/main/java/com/fhsh/daitda/ai/infrastructure/external/dto/DeliveryResponse.java
@@ -0,0 +1,30 @@
+package com.fhsh.daitda.ai.infrastructure.external.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+public class DeliveryResponse {
+	public record DeliveryRoutes (
+		List<DeliveryRoute> routeInfo
+	){}
+	public record DeliveryRoute(
+		UUID deliveryRouteId,
+		UUID deliveryId,
+		String status,
+		Integer sequence,
+		UUID srcNodeId,
+		String srcNodeType,
+		UUID destNodeId,
+		String destNodeType,
+		Integer duration,
+		Double distance,
+		Integer estimatedDuration,
+		Double estimatedDistance,
+		UUID deliveryManagersId,
+		LocalDateTime createdAt, //OffsetDateTime
+		UUID createdBy,
+		LocalDateTime updatedAt,
+		UUID updatedBy
+	) {}
+}
